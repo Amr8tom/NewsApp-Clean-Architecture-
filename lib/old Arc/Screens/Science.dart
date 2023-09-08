47929e3dd7854e4b6e7c17b9e4b6cb860ada9831
Amr alaa ali <92322929+Amr8tom@users.noreply.github.com>
2023-09-08 15:32:52 +0300

    update

---
+++ lib/old Arc/Screens/Science.dart
@@ -0,0 +1,71 @@
+import 'package:conditional_builder/conditional_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:untitled4/Cubit/NewsCubit.dart';
+import 'package:untitled4/Cubit/NewsStates.dart';
+class Science extends StatelessWidget {
+  const Science({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<NewsCubit,states>(
+      listener: (context,state){},
+      builder: (context,state){
+        return ConditionalBuilder(
+            condition: NewsCubit.get(context).science.length !=0 ,
+            builder: (context)=> ListView.separated(
+                physics:BouncingScrollPhysics(),
+                itemBuilder:(context,index)=>Row(
+                  children: [
+                    Container(
+                      width: 120.0,
+                      height: 120.0,
+                      decoration:BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          image:DecorationImage(
+                              image:NetworkImage("${NewsCubit.get(context).science[index]['urlToImage']}"),
+                              fit: BoxFit.cover
+                          )
+
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 120,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Expanded(
+                              child: Text("${NewsCubit.get(context).science[index]['title']}",
+                                style: TextStyle(fontWeight:FontWeight.bold),
+                                maxLines: 4,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text("${NewsCubit.get(context).science[index]['publishedAt']}"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                separatorBuilder:(context,index)=> Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.grey[300],width: double.infinity,height: 1,),
+                ),
+                itemCount: NewsCubit.get(context).science.length
+            ),
+            fallback: (context)=>Center(
+              child:CircularProgressIndicator(),
+            )
+        );
+      },
+    );
+  }
+}
